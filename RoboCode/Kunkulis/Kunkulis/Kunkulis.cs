3d149768963d24ba160520131ec44c9bbf16ad71
Kunkulis <44892092+Kunkulis@users.noreply.github.com>
2020-07-01 07:28:28 +0300

    Update Kunkulis.cs

--- RoboCode/Kunkulis/Kunkulis/Kunkulis.cs
+++ RoboCode/Kunkulis/Kunkulis/Kunkulis.cs
@@ -1,69 +1,95 @@
 using System;
 using Robocode;
+using System.Drawing;
 
 namespace ARE
 {
-    public class Kunkulis : Robot
+    public class Kunkulis : AdvancedRobot
     {
         Random rnd = new Random();
         double previousEnergy = 100;
         int movementDirection = 1;
         int gunDirection = 1;
+
+
+
         public override void Run()
         {
+            SetColors(Color.LimeGreen, Color.Black, Color.DimGray);
+
             double height = this.BattleFieldHeight;
             double width = this.BattleFieldWidth;
-
-
 
             while (true)
             {
 
-                int turn = rnd.Next(10, 181);
-                double horiz = rnd.Next(0, Convert.ToInt32(width));
-                double vertic = rnd.Next(0, Convert.ToInt32(height));
+                int turn = rnd.Next(0, 120);
+                double forward = rnd.Next(100, 500);
+                double revers = rnd.Next(100, 500);
 
                 int direction = rnd.Next(1, 3);
+                int trn = rnd.Next(1, 3);
 
-                if (direction == 1)
+                if (direction == 2)
                 {
-                    Ahead(horiz);
-                    TurnRight(turn);
+                    SetAhead(forward);
                 }
                 else
                 {
-                    Ahead(vertic);
-                    TurnLeft(turn);
+                    SetBack(revers);
                 }
 
-                // Turn the robot 90 degrees
+                if (trn == 2)
+                {
+                    SetTurnLeft(turn);
+                }
+                else
+                {
+                    SetTurnRight(turn);
+                }
 
+                Execute();
+                while (DistanceRemaining > 0 && TurnRemaining > 0)
+                {
+                    Execute();
+                }
 
-                // Our robot will move along the borders of the battle field
-                // by repeating the above two statements.
             }
         }
 
         public override void OnScannedRobot(ScannedRobotEvent e)
         {
-            TurnRight(e.Bearing + 90 - 30 * movementDirection);
+            SetTurnRadarRight(Heading - RadarHeading + e.Bearing);
+            //TurnRight(e.Bearing + 90 - 30 * movementDirection);
 
-            double changeInEnergy = previousEnergy - e.Energy;
-            if (changeInEnergy > 0 && changeInEnergy <= 3)
-            {
-                movementDirection = -movementDirection;
-                Ahead((e.Distance / 4 + 25) * movementDirection);
-            }
+            //double changeInEnergy = previousEnergy - e.Energy;
+            //if (changeInEnergy > 0 && changeInEnergy <= 3)
+            //{
+            //    movementDirection = -movementDirection;
+            //    Ahead((e.Distance / 4 + 25) * movementDirection);
+            //}
 
-            gunDirection = -gunDirection;
-            TurnGunRight(99999 * gunDirection);
+            //gunDirection = -gunDirection;
+            //TurnGunRight(99999 * gunDirection);
             //if (Energy => 75)
             //{
+
             Fire(2);
 
-            previousEnergy = e.Energy;
+            //previousEnergy = e.Energy;
 
             //}
+        }
+
+        public override void OnHitWall(HitWallEvent e)
+        {
+            SetBack(100);
+            SetTurnLeft(70);
+            Execute();
+            while (DistanceRemaining > 0 && TurnRemaining > 0)
+            {
+                Execute();
+            }
         }
     }
 }
