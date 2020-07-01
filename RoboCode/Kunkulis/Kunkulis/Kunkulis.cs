4c7c14c8ad13ec184d3cdbdb358124bd6c4c1a5d
Kunkulis <44892092+Kunkulis@users.noreply.github.com>
2020-07-01 17:37:52 +0300

    Update Kunkulis.cs

--- RoboCode/Kunkulis/Kunkulis/Kunkulis.cs
+++ RoboCode/Kunkulis/Kunkulis/Kunkulis.cs
@@ -1,21 +1,23 @@
 using System;
 using Robocode;
 using System.Drawing;
+using Robocode.Util;
 
 namespace ARE
 {
     public class Kunkulis : AdvancedRobot
     {
         Random rnd = new Random();
-        double previousEnergy = 100;
-        int movementDirection = 1;
+        double previousEnemyEnergy = 100;
+        double energyChange = 0.0;
+        int _direction = 1;
         int gunDirection = 1;
 
 
 
         public override void Run()
         {
-            SetColors(Color.LimeGreen, Color.Black, Color.DimGray);
+            SetColors(Color.LimeGreen, Color.Black, Color.DimGray, Color.Firebrick, Color.WhiteSmoke);
 
             double height = this.BattleFieldHeight;
             double width = this.BattleFieldWidth;
@@ -23,73 +25,83 @@ namespace ARE
             while (true)
             {
 
-                int turn = rnd.Next(0, 120);
-                double forward = rnd.Next(100, 500);
-                double revers = rnd.Next(100, 500);
+                //int turn = rnd.Next(0, 120);
+                //double forward = rnd.Next(100, 500);
+                //double revers = rnd.Next(100, 500);
 
-                int direction = rnd.Next(1, 3);
-                int trn = rnd.Next(1, 3);
+                //int direction = rnd.Next(1, 3);
+                //int trn = rnd.Next(1, 3);
 
-                if (direction == 2)
-                {
-                    SetAhead(forward);
-                }
-                else
-                {
-                    SetBack(revers);
-                }
+                TurnRadarRight(Double.PositiveInfinity);
 
-                if (trn == 2)
-                {
-                    SetTurnLeft(turn);
-                }
-                else
-                {
-                    SetTurnRight(turn);
-                }
+                //if (direction == 2)
+                //{
+                //    SetAhead(forward);
+                //}
+                //else
+                //{
+                //    SetBack(revers);
+                //}
 
-                Execute();
-                while (DistanceRemaining > 0 && TurnRemaining > 0)
-                {
-                    Execute();
-                }
+                //if (trn == 2)
+                //{
+                //    SetTurnLeft(turn);
+                //}
+                //else
+                //{
+                //    SetTurnRight(turn);
+                //}
+
+                //Execute();
+                //while (DistanceRemaining > 0 && TurnRemaining > 0)
+                //{
+                //    Execute();
+                //}
 
             }
         }
 
         public override void OnScannedRobot(ScannedRobotEvent e)
         {
-            SetTurnRadarRight(Heading - RadarHeading + e.Bearing);
-            //TurnRight(e.Bearing + 90 - 30 * movementDirection);
+            SetTurnRadarRightRadians(Utils.NormalRelativeAngle(e.BearingRadians + HeadingRadians - RadarHeadingRadians));
+            SetTurnGunRightRadians(Utils.NormalRelativeAngle(e.BearingRadians + HeadingRadians - GunHeadingRadians));
 
-            //double changeInEnergy = previousEnergy - e.Energy;
-            //if (changeInEnergy > 0 && changeInEnergy <= 3)
-            //{
-            //    movementDirection = -movementDirection;
-            //    Ahead((e.Distance / 4 + 25) * movementDirection);
-            //}
+            SetTurnRight(e.Bearing + 90 - 30 * _direction);
 
-            //gunDirection = -gunDirection;
-            //TurnGunRight(99999 * gunDirection);
-            //if (Energy => 75)
-            //{
+            energyChange = previousEnemyEnergy - e.Energy;
+            previousEnemyEnergy = e.Energy;
 
-            Fire(2);
+            if (e.Distance <= 300)
+            {
+                SetTurnRight(e.Bearing - 90);
+                SetBack(100);
+            }
 
-            //previousEnergy = e.Energy;
+            if (energyChange > 0 && energyChange <= 3)
+            {
+                _direction = (e.Distance <= 300 && _direction < 0) ? _direction : -_direction;
+                SetAhead((e.Distance / 4 + 25) * _direction);
+            }
 
-            //}
+
+            if (Energy >= 75 || e.Distance <= 50)
+            {
+                Fire(3);
+            }
+            else if ((Energy < 75 && Energy > 50) || (e.Distance > 50 && e.Distance < 75))
+            {
+                Fire(2);
+            }
+            else
+            {
+                Fire(1);
+            }
         }
 
         public override void OnHitWall(HitWallEvent e)
         {
-            SetBack(100);
-            SetTurnLeft(70);
-            Execute();
-            while (DistanceRemaining > 0 && TurnRemaining > 0)
-            {
-                Execute();
-            }
+            TurnRight(e.Bearing + 90);
+            SetAhead(20);
         }
     }
 }
