5de1197f7b4b0b1137b5a759b97384c622146f5e
Kunkulis <44892092+Kunkulis@users.noreply.github.com>
2020-07-20 21:50:04 +0300

    Update Kunkulis.cs

--- RoboCode/Kunkulis/Kunkulis/Kunkulis.cs
+++ RoboCode/Kunkulis/Kunkulis/Kunkulis.cs
@@ -2,60 +2,23 @@ using System;
 using Robocode;
 using System.Drawing;
 using Robocode.Util;
+using System.Collections.Generic;
 
 namespace ARE
 {
     public class Kunkulis : AdvancedRobot
     {
-        Random rnd = new Random();
         double previousEnemyEnergy = 100;
         double energyChange = 0.0;
-        int _direction = 1;        
+        int _direction = 1;      
 
         public override void Run()
         {
             SetColors(Color.DarkOrange, Color.Black, Color.DimGray, Color.Orange, Color.WhiteSmoke);
 
-            double height = this.BattleFieldHeight;
-            double width = this.BattleFieldWidth;
-
             while (true)
             {
                 TurnRadarRight(Double.PositiveInfinity);
-
-
-                //int turn = rnd.Next(0, 120);
-                //double forward = rnd.Next(100, 500);
-                //double revers = rnd.Next(100, 500);
-
-                //int direction = rnd.Next(1, 3);
-                //int trn = rnd.Next(1, 3);
-
-
-
-                //if (direction == 2)
-                //{
-                //    SetAhead(forward);
-                //}
-                //else
-                //{
-                //    SetBack(revers);
-                //}
-
-                //if (trn == 2)
-                //{
-                //    SetTurnLeft(turn);
-                //}
-                //else
-                //{
-                //    SetTurnRight(turn);
-                //}
-
-                //Execute();
-                //while (DistanceRemaining > 0 && TurnRemaining > 0)
-                //{
-                //    Execute();
-                //}
 
             }
         }
@@ -63,52 +26,50 @@ namespace ARE
 
         public override void OnScannedRobot(ScannedRobotEvent e)
         {
-            double firePower = Math.Min(400 / e.Distance, 3);
-            double bulletSpeed = 20 - firePower * 3;
+            //---Linear Targeting---            
+            double bulletPower = Math.Min((400 / e.Distance), 3);
+            double myX = X;
+            double myY = Y;
+            double absBearing = e.BearingRadians + HeadingRadians;
+            double eX = X + e.Distance * Math.Sin(absBearing); //Enemy future X
+            double eY = Y + e.Distance * Math.Cos(absBearing); //Enemy future Y
+            double enemyHeading = e.HeadingRadians;
+            double enemyVelocity = e.Velocity;
+            double deltaTime = 0;
+            double battleFieldHeight = BattleFieldHeight,
+                   battleFieldWidth = BattleFieldWidth;
+            double enemyDistance = e.Distance;
+
+            while ((deltaTime++) * (20.0 - 3.0 * bulletPower) < Math.Sqrt(Math.Pow((eX - myX), 2) + Math.Pow((eY - myY), 2)))
+            {
+                eX += Math.Sin(enemyHeading) * enemyVelocity;
+                eY += Math.Cos(enemyHeading) * enemyVelocity;
+                if (eX < 18.0 || eY < 18.0 || eX > battleFieldWidth - 18.0 || eY > battleFieldHeight - 18.0)
+                {
+                    eX = Math.Min(Math.Max(18.0, eX), battleFieldWidth - 18.0);
+                    eY = Math.Min(Math.Max(18.0, eY), battleFieldHeight - 18.0);
+                    break;
+                }
+            }
+            double theta = Utils.NormalAbsoluteAngle(Math.Atan2(eX - X, eY - Y));
+            SetTurnRadarRightRadians(Utils.NormalRelativeAngle(absBearing - RadarHeadingRadians));
+            SetTurnGunRightRadians(Utils.NormalRelativeAngle(theta - GunHeadingRadians));
+            Fire(bulletPower);
+            //---Linear Targeting---
+
+
+            double bulletSpeed = 20 - bulletPower * 3;
             long time = (long)(e.Distance / bulletSpeed);
 
-            double absBearing = e.BearingRadians + HeadingRadians;
-            double laterVelocity = e.Velocity * Math.Sin(e.HeadingRadians - absBearing);
-            double gunTurnAmt;            
 
-            Console.WriteLine("absBearing=" + absBearing + " laterVel=" + laterVelocity);
+            //SetTurnRadarRightRadians(Utils.NormalRelativeAngle(e.BearingRadians + HeadingRadians - RadarHeadingRadians));
 
-            SetTurnRadarRightRadians(Utils.NormalRelativeAngle(e.BearingRadians + HeadingRadians - RadarHeadingRadians));
-            //SetTurnRadarLeftRadians(RadarTurnRemainingRadians);
-            Console.WriteLine("RadarTurnRemainging=" + RadarTurnRemainingRadians);
-            SetTurnGunRightRadians(Utils.NormalRelativeAngle(e.BearingRadians + HeadingRadians - GunHeadingRadians));
+            //SetTurnGunRightRadians(Utils.NormalRelativeAngle(e.BearingRadians + HeadingRadians - GunHeadingRadians));
 
             SetTurnRight(e.Bearing + 90 - 30 * _direction);
 
             energyChange = previousEnemyEnergy - e.Energy;
             previousEnemyEnergy = e.Energy;
-
-            //if (e.Distance > 150)
-            //{
-            //    Console.WriteLine("Distance=" + e.Distance);
-            //    gunTurnAmt = Utils.NormalRelativeAngle(absBearing - GunHeadingRadians + laterVelocity / 22);//amount to turn our gun, lead just a little bit
-            //    Console.WriteLine("gunTurnAmt=" + gunTurnAmt);
-            //    TurnGunRightRadians(gunTurnAmt);
-            //    TurnRightRadians(Utils.NormalRelativeAngle(absBearing - GunHeadingRadians + laterVelocity / Velocity));//drive towards the enemies predicted future location
-            //    Console.WriteLine("TurnRightRadians=" + Utils.NormalRelativeAngle(absBearing - GunHeadingRadians + laterVelocity / Velocity));
-            //    SetAhead((e.Distance - 140) * _direction);
-            //    Console.WriteLine("Ahead=" + (e.Distance - 140) * _direction);
-            //    SetFire(firePower);
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Distance=" + e.Distance);
-            //    gunTurnAmt = Utils.NormalRelativeAngle(absBearing - GunHeadingRadians + laterVelocity / 15);//amount to turn our gun, lead just a little bit
-            //    Console.WriteLine("gunTurnAmt=" + gunTurnAmt);
-            //    TurnGunRightRadians(gunTurnAmt);
-            //    TurnLeft(-90 - e.Bearing);//turn perpendicular to the enemy
-            //    Console.WriteLine("TurnLeft=" + (-90 - e.Bearing));
-            //    SetAhead((e.Distance - 140) * _direction);
-            //    Console.WriteLine("Ahead=" + (e.Distance - 140) * _direction);
-            //    SetFire(firePower);
-            //}
-
-            Console.WriteLine("Andrejs part");
 
             if (e.Distance <= 300)
             {
@@ -121,10 +82,10 @@ namespace ARE
                 _direction = (e.Distance <= 300 && _direction < 0) ? _direction : -_direction;
                 SetAhead((e.Distance / 4 + 25) * _direction);
             }
-            if (GunHeat == 0 && Math.Abs(GunTurnRemaining) < 10)
-            {                 
-                SetFire(firePower);
-            }
+            //if (GunHeat == 0 && Math.Abs(GunTurnRemaining) < 10)
+            //{
+            //    SetFire(bulletPower);
+            //}
         }
 
         public override void OnHitWall(HitWallEvent e)
@@ -132,11 +93,6 @@ namespace ARE
             SetTurnRight(e.Bearing + 135);
             SetAhead(200);
         }
-        public override void OnHitByBullet(HitByBulletEvent e)
-        {
-            SetTurnRight(e.Bearing + 135);
-            SetAhead(300);
-        }
-
     }
+   
 }
