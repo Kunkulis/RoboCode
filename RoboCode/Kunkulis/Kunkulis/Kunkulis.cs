66436070682156d670aca467c2fabcb0a1c37f14
Kunkulis <44892092+Kunkulis@users.noreply.github.com>
2020-07-30 16:39:07 +0300

    Update Kunkulis.cs

--- RoboCode/Kunkulis/Kunkulis/Kunkulis.cs
+++ RoboCode/Kunkulis/Kunkulis/Kunkulis.cs
@@ -15,6 +15,8 @@ namespace ARE
 
         public override void Run()
         {
+            
+            TurnRight(-Heading);            
             SetColors(Color.DarkOrange, Color.Black, Color.DimGray, Color.Orange, Color.WhiteSmoke);
             while (true)
             {
@@ -27,41 +29,42 @@ namespace ARE
             energyChange = previousEnemyEnergy - e.Energy;
             previousEnemyEnergy = e.Energy;
 
-            if (e.Distance <= 250)
-            {
+            //if (e.Distance <= 250)
+            //{
                 Raming(e);
-            }
-            else if(e.Distance>250)
-            {
-                LinearTargeting(e);
-            }            
+            //}
+            //else if (e.Distance > 250)
+            //{
+            //    LinearTargeting(e);
+            //}
         }
 
         public override void OnHitWall(HitWallEvent e)
         {
             SetTurnRight(e.Bearing + 90);
             SetAhead(50);
-            _direction = -_direction;           
+            _direction = -_direction;
         }
 
         public void Raming(ScannedRobotEvent e)
         {
             double absBearing = e.BearingRadians + HeadingRadians;
+            Console.WriteLine("e.BR={0} HR={1}", e.BearingRadians, HeadingRadians);
             double turn = absBearing + Math.PI / 2;
-            Random r = new Random();
-            double ran = r.NextDouble();
+            //Random r = new Random();
+            //double ran = r.NextDouble();
             SetColors(Color.Red, Color.Black, Color.DimGray, Color.Orange, Color.WhiteSmoke);
             double bulletPower = 3;
             double bulletSpeed = 20 - 3 * bulletPower;
             double bulletDamage = 4 * bulletPower;
             turn -= Math.Max(0.5, (1 / e.Distance) * 100) * _direction;
+            Console.WriteLine("e.D={0}", e.Distance);
 
             SetTurnRightRadians(Utils.NormalRelativeAngle(turn - HeadingRadians));
 
             //This line makes us slow down when we need to turn sharply.
 
             MaxVelocity = (400 / TurnRemaining);
-            Console.WriteLine("MaxVelocity={0}", 400 / TurnRemaining);
 
             SetAhead(100 * _direction);
 
@@ -69,6 +72,7 @@ namespace ARE
             double enemyHeading = e.HeadingRadians;
             double enemyHeadingChange = enemyHeading - oldEnemyHeading;
             oldEnemyHeading = enemyHeading;
+            Console.WriteLine("enemyHeading={0} enHeadChang={1} oldEnHead={2}", e.HeadingRadians, enemyHeadingChange, oldEnemyHeading);
 
             /*This method of targeting is know as circular targeting; you assume your enemy will
              *keep moving with the same speed and turn rate that he is using at fire time.The 
@@ -80,9 +84,12 @@ namespace ARE
             Console.WriteLine("predictedX={0} X={1} + e.Distance={2} * Math.Sin(absBearing)={3} | absBearing={4}", predictedX, X, e.Distance, Math.Sin(absBearing), absBearing);
             double predictedY = Y + e.Distance * Math.Cos(absBearing);
             Console.WriteLine("predictedY={0} Y={1} + e.Distance={2} * Math.Cos(absBearing)={3} | absBearing={4}", predictedY, Y, e.Distance, Math.Cos(absBearing), absBearing);
+            Console.WriteLine("e.Vel={0}",e.Velocity);
 
+            int i = 0;
             while ((++deltaTime) * bulletSpeed < Math.Sqrt(Math.Pow((predictedX - X), 2) + Math.Pow((predictedY - Y), 2)))
             {
+                Console.WriteLine("Nr={0}", i);
                 //Add the movement we think our enemy will make to our enemy's current X and Y
                 Console.WriteLine("deltaTime={0}*bulletSpeed={1}", deltaTime, bulletSpeed);
                 Console.WriteLine("Math.Sqrt(Math.Pow((predictedX - X), 2)={0} + Math.Pow((predictedY - Y), 2))={1} | X={2} Y={3}", Math.Pow((predictedX - X), 2), Math.Pow((predictedY - Y), 2), X, Y);
@@ -92,11 +99,13 @@ namespace ARE
 
                 //Find our enemy's heading changes.
                 enemyHeading += enemyHeadingChange;
+                Console.WriteLine("enemyHeading={0}", enemyHeading);
 
                 //If our predicted coordinates are outside the walls, put them 18 distance units away from the walls as we know 
                 //that that is the closest they can get to the wall (Bots are non-rotating 36*36 squares).
                 predictedX = Math.Max(Math.Min(predictedX, BattleFieldWidth - 18), 18);
                 predictedY = Math.Max(Math.Min(predictedY, BattleFieldHeight - 18), 18);
+                ++i;
             }
             //Find the bearing of our predicted coordinates from us.
             double aim = Utils.NormalAbsoluteAngle(Math.Atan2(predictedX - X, predictedY - Y));
